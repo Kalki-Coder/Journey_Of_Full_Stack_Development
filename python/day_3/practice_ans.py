9b0366ed1e9a7719e345feaf17b18d1fe2040bf7
Kalki-Coder <mithlesh.guptag0329@gmail.com>
2025-12-25 16:03:11 +0530

    day-3

---
+++ python/day_3/practice_ans.py
@@ -0,0 +1,66 @@
+# print('True == True: ', True == True) #True
+# print('True == False: ', True == False) #False
+# print('False == False:', False == False) #True
+# print(bin(1))
+# print(bin(2))
+# print(bin(3))
+# print(bin(4))
+# print(bin(5))
+# print(bin(6))
+
+# age = 25
+# height = 163.5
+# complex_no = 1 + 1j
+# height = int(input("enter the height of the triangle :- "))
+# base = int(input("enter the base of the triangle :- "))
+# area = int((base*height)/2)
+# # print(f"the area of the triangle given is {area}")
+
+# a_side = int(input("enter the length of the a side of triangle :- "))
+# b_side = int(input("enter the length of the b side of triangle :- "))
+# c_side = int(input("enter the length of the c side of triangle :- "))
+# perimeter_triangle = a_side + b_side + c_side
+# print(f"the perimeter of the triangle given is {perimeter_triangle}")
+
+# a_side = int(input("enter the length of the a side of rectangle :- "))
+# b_side = int(input("enter the length of the b side of rectangle :- "))
+# perimeter = (a_side+b_side)*2
+# area = a_side*b_side
+# print(f"area of the rectangle is {area} and perimeter of the rectangle is {perimeter}")
+# radius = int(input("enter the radius of the given circle :- "))
+# circumferece = math.pi*2*radius
+# area = math.pi*(radius**2)
+# print(f"the circumference of the given circle is {circumferece} and area is {area}")
+# import math
+# x1,y1,x2,y2 = (2,2,6,10)
+# euclidean_distance = math.sqrt((y2-y1)**2 + (x2-x1)**2)
+# slope = (y2 - y1)/(x2 - x1)
+# print(f"the slope the given equation will always be {int(slope)} as it is a straight line and the euclidean distance is {euclidean_distance}")
+# print(int(9.8))
+
+# hours, rph = map(int,input("enter the hours you worked and the rph decided by the govt. :- ").split())
+# print(f"the person who worked {hours}h will earn {hours*rph}$ as per the {rph}$ per hour decided by the ruling govt.")
+
+# years_onelived = int(input("enter your age in years :- "))
+# print(f"as per the research data a person can live on an average 100 years that why you have lived {years_onelived*60*60*24*365} seconds and {(100-years_onelived)*60*60*24*365} seconds left for rest of your life")
+# print(
+# """1 1 1 1 1
+# 2 1 2 4 8
+# 3 1 3 9 27
+# 4 1 4 16 64
+# 5 1 5 25 125"""
+# )
+
+# user_input = int(input("enter the digit in your mind :- "))
+# print("even" if user_input % 2 == 0 else "odd")
+
+# print(str(float(len('python'))))
+
+# print('on' not in 'python' and  'on' not in 'jargon')
+# print('on' not in 'python' or  'on' in 'jargon')
+# saying = 'i hope this course is not full of jargon'
+# print('jargon' in saying)
+
+# x = int(input("enter the value of x to find out y :- "))
+# y = x**2 + 6*x + 9
+# print(f"the root of the equation is {x}" if y==0 else f"try again the entered value of {x} is not the root")
