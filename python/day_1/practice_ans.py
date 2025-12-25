74aea818840576fe6414f43ab4102547506405b0
Kalki-Coder <mithlesh.guptag0329@gmail.com>
2025-12-25 16:22:55 +0530

    day-1

--- python/day_1/practice_ans.py
+++ python/day_1/practice_ans.py
@@ -1,1 +1,15 @@
-# Null
+# python -V tocheck python version
+# print(3 + 4) # = 7
+# print(3 - 4) # = -1
+# print(3 * 4) # = 12
+# print(3 % 4) # = 3
+# print(3 / 4) # = 0.75
+# print(3 ** 4) # = 81
+# print(3 // 4) # = 0
+
+your_name = 'Mithlesh Gupta'
+family_name = 'Gupta'
+country = "India"
+doing_for_entertainment = 'I am enjoying 30 days of python'
+
+print(type(your_name))
